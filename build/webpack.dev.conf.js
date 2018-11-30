b04912594bda35f787a1b7d5d69fef330e2d558b
Flynn Davies <fdavies.freelance@gmail.com>
2018-11-30 16:15:20 +0000

    biiig progress

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -16,7 +16,10 @@ const PORT = process.env.PORT && Number(process.env.PORT)
 
 const devWebpackConfig = merge(baseWebpackConfig, {
   module: {
-    rules: utils.styleLoaders({ sourceMap: config.dev.cssSourceMap, usePostCSS: true })
+    rules: utils.styleLoaders({
+      sourceMap: config.dev.cssSourceMap,
+      usePostCSS: true
+    })
   },
   // cheap-module-eval-source-map is faster for development
   devtool: config.dev.devtool,
@@ -25,9 +28,10 @@ const devWebpackConfig = merge(baseWebpackConfig, {
   devServer: {
     clientLogLevel: 'warning',
     historyApiFallback: {
-      rewrites: [
-        { from: /.*/, to: path.posix.join(config.dev.assetsPublicPath, 'index.html') },
-      ],
+      rewrites: [{
+        from: /.*/,
+        to: path.posix.join(config.dev.assetsPublicPath, 'index.html')
+      }, ],
     },
     hot: true,
     contentBase: false, // since we use CopyWebpackPlugin.
@@ -35,9 +39,12 @@ const devWebpackConfig = merge(baseWebpackConfig, {
     host: HOST || config.dev.host,
     port: PORT || config.dev.port,
     open: config.dev.autoOpenBrowser,
-    overlay: config.dev.errorOverlay
-      ? { warnings: false, errors: true }
-      : false,
+    overlay: config.dev.errorOverlay ?
+      {
+        warnings: false,
+        errors: true
+      } :
+      false,
     publicPath: config.dev.assetsPublicPath,
     proxy: config.dev.proxyTable,
     quiet: true, // necessary for FriendlyErrorsPlugin
@@ -47,27 +54,25 @@ const devWebpackConfig = merge(baseWebpackConfig, {
   },
   plugins: [
     new StyleLintPlugin(),
-    new webpack.DefinePlugin({
-      'process.env': require('../config/dev.env')
-    }),
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NamedModulesPlugin(), // HMR shows correct file names in console on update.
-    new webpack.NoEmitOnErrorsPlugin(),
-    // https://github.com/ampedandwired/html-webpack-plugin
-    new HtmlWebpackPlugin({
-      filename: 'index.html',
-      template: 'index.html',
-      inject: true
-    }),
-    // copy custom static assets
-    new CopyWebpackPlugin([
-      {
-        from: path.resolve(__dirname, '../static'),
-        to: config.dev.assetsSubDirectory,
-        ignore: ['.*']
-      }
-    ])
-  ]
+  new webpack.DefinePlugin({
+    'process.env': require('../config/dev.env')
+  }),
+  new webpack.HotModuleReplacementPlugin(),
+  new webpack.NamedModulesPlugin(), // HMR shows correct file names in console on update.
+  new webpack.NoEmitOnErrorsPlugin(),
+  // https://github.com/ampedandwired/html-webpack-plugin
+  new HtmlWebpackPlugin({
+    filename: 'index.html',
+    template: 'index.html',
+    inject: true
+  }),
+  // copy custom static assets
+  new CopyWebpackPlugin([{
+    from: path.resolve(__dirname, '../static'),
+    to: config.dev.assetsSubDirectory,
+    ignore: ['.*']
+  }])
+]
 })
 
 module.exports = new Promise((resolve, reject) => {
@@ -86,9 +91,9 @@ module.exports = new Promise((resolve, reject) => {
         compilationSuccessInfo: {
           messages: [`Your application is running here: http://${devWebpackConfig.devServer.host}:${port}`],
         },
-        onErrors: config.dev.notifyOnErrors
-        ? utils.createNotifierCallback()
-        : undefined
+        onErrors: config.dev.notifyOnErrors ?
+          utils.createNotifierCallback() :
+          undefined
       }))
 
       resolve(devWebpackConfig)
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,15 +1,67 @@
 <template>
-  <div class="hello center-content">
-    <h1>{{ msg }}</h1>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-12">
+        <h1 class="h1">Genre Hunt</h1>
+        <hr>
+      </div>
+    </div>
+    <div class="row justify-content-center">
+      <div class="col-5">
+        <input class="genre--search-bar" type="text" placeholder="Search for a genre..." v-model="searchEntry" @change="searchGenres">
+      </div>
+    </div>
+    <div class="row search-btns">
+      <div class="col-3 search-btns--btn"><h1>Random Genre</h1></div>
+      <div class="col-3 search-btns--btn"><h1>Recently Updated</h1></div>
+      <div class="col-3 search-btns--btn"><h1>Mix Challenge</h1></div>
+    </div>
+    <div class="genre--container row">
+      <div v-for="data in genreJson" class="genre--square col-2">
+        <h1 class="h1">{{data.name}}</h1>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+// random color picker
+import json from '../lib/genres.json'
+
+window.onload = function (e) {
+  const genreSquares = document.getElementsByClassName('genre--square')
+  let usedColors = []
+  let genreNameArray = []
+  Array.from(genreSquares).forEach(function (genreSquare, index) {
+    genreNameArray.push(genreSquare.h1.textContent)
+    console.log(genreNameArray)
+    let randomColor = randomColorGen()
+    if (!usedColors.includes(randomColor)) {
+      genreSquare.style.backgroundColor = randomColor
+    }
+    usedColors.push(randomColor)
+  })
+}
+
+function searchGenres(searchString){
+}
+
+function randomColorGen () {
+  var letters = '0123456789ABCD'
+  var color = '#'
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 14)]
+  }
+  // console.log(color)
+  return color
+}
+
 export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome'
+      searchEntry: '',
+      genreJson: json
     }
   }
 }
@@ -17,18 +69,43 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss" scoped>
-h1, h2 {
-  font-weight: normal;
+.row {
+  align-content: center;
+  align-items: center;
 }
-ul {
-  list-style-type: none;
+.genre--container {
+  display: flex;
+  flex-wrap: wrap;
+  align-content: flex-start;
+}
+.genre--search-bar {
+  display: flex;
+  width: 100%;
+  align-self: center;
+  padding: 10px 15px;
+  margin-bottom: 20px;
+  font-size: 1.5rem;
+  border: 2px solid slategrey;
+  border-radius: 5px;
+}
+.search-btns{
+  justify-content: space-evenly;
+  margin:30px 0;
+}
+.genre--square {
+  background: lightblue;
+  height: calc(100vw / 6);
+  display: flex;
+  align-content: center;
+  justify-content: center;
+  align-items: center;
+  color: #fff;
+  margin: 0;
   padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+
+  .h1 {
+    width: 100%;
+    padding: 20px 0;
+  }
 }
 </style>
